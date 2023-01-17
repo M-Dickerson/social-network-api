923963c7dfe2dcaab7550bc11233dbc7db2c8fe6
D <Dickerson.Morgan@outlook.com>
2023-01-17 10:25:43 -0500

    added reaction model

---
+++ models/reaction.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+// 
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectID,
+            default: () => new Types.ObjectID(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+// export
+module.exports = reactionSchema;
