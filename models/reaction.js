b056eee4204d3039406cd5c783bf58bb097f284c
D <Dickerson.Morgan@outlook.com>
2023-01-17 11:55:11 -0500

    added user model

--- models/reaction.js
+++ models/reaction.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 // creates the reaction schema
 const reactionSchema = new Schema(
     {
---
+++ models/user.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+// creates the user schema
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "user",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        // id: false,
+    }
+);
+
+const user = model("user", userSchema);
+
+module.exports = user;
